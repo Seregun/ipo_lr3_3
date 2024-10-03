5b54e6064e2a13b597b96e8f96293e11df6d92fc
Seregun <seregunmgp@gmail.com>
2024-10-03 09:59:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+day = int(input("Введите день: ")) # Ввод дня
+month = int(input("Введите месяц: ")) # Ввод месяца
+if (day < 1 or month < 1):
+    print("Невеная дата!")
+else:
+    if (month == 3 and day >= 1) or (month == 4) or (month == 5) or (month == 6 and day <= 31): # Проверка на время года "Весна"
+       season = "Весна" # Время года "Весна"
+    elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 9 and day <= 31): # Проверка на время года "Лето"
+        season = "Лето" # Время года "Лето"
+    elif (month == 9 and day >= 1) or (month == 10) or (month == 11) or (month == 12 and day <= 30): # Проверка на время года "Осень"
+       season = "Осень" # Время года "Осень"
+    else: # Если не "Весна", "Лето" или "Осень"
+        season = "Зима" # Время года "Зима"
+    print(f"Эта дата относится к времени года: {season}") # Вывод времени года
